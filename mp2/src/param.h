e0b1bf20787b2dc1ac644b1e4137574e550573fa
unw9527 <1041593558@qq.com>
2022-10-16 19:01:30 +0800

    add: param.h

---
+++ mp2/src/param.h
@@ -0,0 +1,51 @@
+#ifndef PARAM_H
+#define PARAM_H
+
+#include <stdlib.h>
+
+#define MSS             1000
+#define BASE            1000
+#define MAX_QUEUE_SIZE  512
+#define TIMEOUT         1000
+
+enum packet_t{
+    DATA,
+    ACK,
+    FIN,
+    FINACK
+};
+
+enum status_t{
+    SLOW_START = 10,
+    CONGESTION_AVOID,
+    FAST_RECOVERY
+};
+
+typedef struct{
+	int 	    data_size;
+	uint64_t 	seq_idx;
+	uint64_t    ack_idx;
+	packet_t    pkt_type;
+	char        data[MSS];
+}packet;
+
+struct compare {
+    bool operator()(packet a, packet b) {
+        return  a.seq_idx > b.seq_idx; 
+    }
+};
+
+extern void diep(const char *s);
+
+/**
+ * @brief Reports errors and exits
+ * 
+ * @param s 
+ */
+void diep(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+#endif
+
